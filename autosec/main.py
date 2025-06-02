a1e5aa95a587ddc0819b4f72c22cfbb79d0a8e9d
tweekes56 <tweekes07@gmail.com>
2025-06-02 11:39:49 -0400

    Allowed notifications to appear on your phone through IFTTT if process id dected

--- autosec/main.py
+++ autosec/main.py
@@ -1,6 +1,10 @@
 import psutil
 import time
 import os
+import requests
+
+
+url = "https://maker.ifttt.com/trigger/autosec_alert/with/key/czfuWWBFmr064bXacqKeY"
 
 # List of suspicious keywords (you can expand this)
 SUSPICIOUS_KEYWORDS = ["powershell", "netcat", "mimikatz", "cmd", "ncat"]
@@ -11,6 +15,14 @@ log_path = os.path.join(os.path.dirname(__file__), "logs", "alerts.log")
 def log_event(message):
     with open(log_path, "a") as f:
         f.write(f"[{time.ctime()}] {message}\n")
+
+
+def send_ifttt_alert(pname, pid):
+    data = {"value1": pname, "value2": str(pid)}
+    try:
+        requests.post(url, json=data)
+    except Exception as e:
+        print(f"[!] Failed to send IFTTT alert: {e}")
 
 
 def scan_processes():
@@ -36,10 +48,12 @@ def scan_processes():
                         print(f"[x] Process: {pname} (PID: {pid}) was killed")
 
                         log_event(f"Suspicious process was suspended: {pname} (PID: {pid})")
+                        send_ifttt_alert(pname, pid)
+
 
                     except Exception as e:
                         print(f"[!] Failed to kill process {pname} (PID: {pid}): {e}")
-                        
+
                         log_event(f"Suspicious process failed to suspend: {pname} (PID: {pid})")
 
         except (psutil.NoSuchProcess, psutil.AccessDenied):
