7c7ec2b3b8ed4c3f4e0054165561ba02fd04b070
tweekes56 <tweekes07@gmail.com>
2025-06-01 23:16:57 -0400

    Added process auto-kill and centralized event logging

--- autosec/main.py
+++ autosec/main.py
@@ -6,6 +6,12 @@ import os
 SUSPICIOUS_KEYWORDS = ["powershell", "netcat", "mimikatz", "cmd", "ncat"]
 
 flagged_pids = set()
+log_path = os.path.join(os.path.dirname(__file__), "logs", "alerts.log")
+
+def log_event(message):
+    with open(log_path, "a") as f:
+        f.write(f"[{time.ctime()}] {message}\n")
+
 
 def scan_processes():
     print("[*] Scanning running processes...\n")
@@ -18,13 +24,23 @@ def scan_processes():
             for keyword in SUSPICIOUS_KEYWORDS:
                 if keyword in pname:
                     if pid in flagged_pids:
-                      continue
-                    else:
-                        flagged_pids.add(pid)
-                        print(f"[!] Suspicious process detected: {pname} (PID: {pid})")
-                        log_path = os.path.join(os.path.dirname(__file__), "logs", "alerts.log")
-                        with open(log_path, "a") as f:
-                            f.write(f"[{time.ctime()}] Suspicious process detected: {pname} (PID: {pid})\n")
+                        continue
+
+                    flagged_pids.add(pid)
+                    print(f"[!] Suspicious process detected: {pname} (PID: {pid})")
+
+                    log_event(f"Suspicious process detected: {pname} (PID: {pid})")
+
+                    try:
+                        proc.terminate()
+                        print(f"[x] Process: {pname} (PID: {pid}) was killed")
+
+                        log_event(f"Suspicious process was suspended: {pname} (PID: {pid})")
+
+                    except Exception as e:
+                        print(f"[!] Failed to kill process {pname} (PID: {pid}): {e}")
+                        
+                        log_event(f"Suspicious process failed to suspend: {pname} (PID: {pid})")
 
         except (psutil.NoSuchProcess, psutil.AccessDenied):
             pass
